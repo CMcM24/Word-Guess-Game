bd5d4a4590ec6225e638b7ae84ab1927e9a1220f
Colin McMahon <cmcmahon1012@gmail.com>
2019-06-13 23:41:15 -0400

    got the win function to work, no alerts that you guessed the movie correctly, adds a +1 to the win counter, and picks a new word. also figured out the embeded audio player, added more movie titles, finalized some styling. also cleaned up the code so its more organized.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,15 @@
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var movieChoices = ["terminator", "diehard", "predator", "kungfury", "alien", "bladerunner"];
+var movieChoices = ["terminator", "diehard", "predator", "kungfury", "alien", "bladerunner", "starshiptroopers", "starwars", "robocop", "truelies", "commando", "thematrix"];
 
-var guessTotal = 10;
+var guessTotal = 12;
 var winsTotal = 0;
 var chosenMovie = "";
+var incorrectChoice = [];
 var movieFilling = [];
 var movieBlanks = [];
 var movieLetters = [];
-
-
-
-// ------------------------------------------------------------------------------ //
-
-function movies() {
-    chosenMovie = movieChoices[Math.floor(Math.random() * movieChoices.length)];
-
-    movieLetters = chosenMovie.split("");
-    movieBlanks = movieLetters.length; 
-
-    for(i = 0; i < movieBlanks; i++) {
-        movieFilling.push("_");
-    }
-
-    document.getElementById("wordblanks").innerHTML = " " + movieFilling.join(" ");
-
-    console.log(chosenMovie);
-    console.log(movieLetters);
-    console.log(movieBlanks);
-    console.log(movieFilling);
-}
-
-
-
-
-movies();
 
 var synthwave = document.getElementById("#synth");
 
@@ -46,6 +20,107 @@ function playSynth(){
 function pauseSynth(){
     synthwave.pause();
 }
+
+// ------------------------------------------------------------------------------ //
+
+function wordGuessStart() {
+    chosenMovie = movieChoices[Math.floor(Math.random() * movieChoices.length)];
+
+    movieLetters = chosenMovie.split("");
+    movieBlanks = movieLetters.length; 
+
+    for(i = 0; i < movieBlanks; i++) {
+        movieFilling.push("_");
+    }
+
+    document.getElementById("wordblanks").innerHTML = " " + movieFilling.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessTotal;
+    document.getElementById("wins").innerHTML = " " + winsTotal;
+
+}
+// ------------------------------------------------------------------------------ //
+
+function keyCheck(letter) {
+    var correct = false; 
+
+    for(var i = 0; i < movieBlanks; i++) {
+        if(chosenMovie[i] == letter) {
+            correct = true;
+        }
+    }
+
+    if(correct) {
+        for(var i = 0; i < movieBlanks; i++) {
+            if(chosenMovie[i] == letter){
+                movieFilling[i] = letter;
+            }
+        }
+    }
+    else if(letter == incorrectChoice[i]){
+        guessTotal++;
+    }
+    else{
+        incorrectChoice.push(letter);
+        guessTotal--;
+    }
+}
+
+// ------------------------------------------------------------------------------ //
+
+function restart (){
+    location.reload();
+}
+
+// ------------------------------------------------------------------------------ //
+
+function nextMovie(){
+    guessTotal = 12;
+    chosenMovie = "";
+    incorrectChoice = [];
+    movieFilling = [];
+    movieBlanks = [];
+    movieLetters = [];
+}
+
+// ------------------------------------------------------------------------------ //
+
+function winning(){
+    if(movieLetters.toString() == movieFilling.toString()){
+        winsTotal++;
+        alert(chosenMovie + "! Correct!")
+        nextMovie();
+        wordGuessStart();
+
+        document.getElementById("wins").innerHTML = " " + winsTotal;
+
+    }
+    else if (guessTotal === 0) {
+
+        alert("Game Over.")
+        restart();
+    }
+
+    document.getElementById("wordblanks").innerHTML = "  " + movieFilling.join(" ");
+    document.getElementById("guessesremaining").innerHTML = "  " + guessTotal;
+}
+
+// ------------------------------------------------------------------------------ //
+
+wordGuessStart();
+
+document.onkeyup = function(event){
+    var letterKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+        keyCheck(letterKey);
+        winning();
+
+    document.getElementById("guessedletters").innerHTML = " " + incorrectChoice.join(" ");
+
+
+
+}
+
+
 
 
 
